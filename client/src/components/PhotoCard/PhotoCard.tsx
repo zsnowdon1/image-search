569a03e931dfbb31ecd8275f00160401cae0faee
Zack Snowdon <zack@Zacks-Air.fios-router.home>
2023-04-23 20:53:51 -0400

    Added card for photo with image and filename

--- client/src/components/PhotoCard/PhotoCard.tsx
+++ client/src/components/PhotoCard/PhotoCard.tsx
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
 import { Photo } from '../../models/models';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 export function PhotoCard(photo: Photo) {
 
     return (
-        <div>
-            <img src={photo.downloadUrl}/>
-        </div>
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="500"
+                    src={photo.downloadUrl}
+                    alt="green iguana"/>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {photo.filename}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     );
 }
